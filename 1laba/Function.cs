66b558fdd1b0ac1dfe59194b0bb7f6fe30bc0caa
s1man <99068419+Banes228@users.noreply.github.com>
2023-05-01 20:42:44 +0300

    Add files via upload

---
+++ 1laba/Function.cs
@@ -0,0 +1,34 @@
+﻿class Function
+{
+
+    static void Main(String[] args)
+    {
+        double sum = 0;
+
+        Console.WriteLine("Введите х: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите кол-во иттераций: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n < 0) {
+            Console.WriteLine("Ошибка! Вы ввели отрицательное кол-во иттераций");
+            System.Environment.Exit(0);
+        }
+        for (int i = 0; i <= n; i++)
+        {
+            x = (Math.Pow(-1, i) * Math.Pow(x, i) / (getFactorial(i)));
+            sum += x;
+            Console.WriteLine("Сумма иттерации № " + i + " = " + sum);            
+        }
+    }
+    public static int getFactorial(int f)
+    {
+        int result = 1;
+        for (int i = 1; i <= f; i++)
+        {
+            result = result * i;
+        }
+        return result;
+    }
+}
